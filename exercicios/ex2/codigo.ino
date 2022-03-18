a30eeeacad24fa4cbe36b624010c6a02c638575f
Larissaoaa <101872083+Larissaoaa@users.noreply.github.com>
2022-03-18 08:55:45 -0300

    Update codigo.ino

--- exercicios/ex2/codigo.ino
+++ exercicios/ex2/codigo.ino
@@ -1,32 +1,19 @@
-const int led1 = 3;
-const int led2 = 4;
-const int botao1 = 6;
-const int botao2 = 7;
+// C++ code
+//
+void setup()
+{
+  pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+}
 
-int var1;
-int var2;
-  
- void setup()
- { 
-    pinMode(led1, OUTPUT);
-    pinMode(led2, OUTPUT);
-    pinMode(botao1, INPUT);
-  	pinMode(botao2, INPUT); 
- }
- 
- void loop()
- {
-    var1 = digitalRead(botao1);
-   	var2 = digitalRead(botao2);
-   	
-   if(var1){
-       digitalWrite(led1,HIGH);
-   }else{
-       digitalWrite(led1,LOW);
-   }
-   if(!var2){
-       digitalWrite(led2, HIGH);
-   }else{
-       digitalWrite(led2, LOW);
-   }
- }
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1 millisecond(s)
+  digitalWrite(3, HIGH);
+  delay(1000); // Wait for 1 millisecond(s)
+  digitalWrite(3, LOW);
+  delay(1000); // Wait for 1 millisecond(s)
+}
